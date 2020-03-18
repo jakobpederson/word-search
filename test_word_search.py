39b0e443c7e2c77a2af34161a440e8f0bf520693
jakobpederson <jakob.pederson+github@gmail.com>
2020-03-18 18:36:44 -0500

    Test: test passing

--- test_word_search.py
+++ test_word_search.py
@@ -1,27 +1,27 @@
-from word_search import is_present
+from word_search import is_present, handle_lists
 
 
 class TestWordSearch():
 
     def test_word_found_in_a_list_of_strings(self):
-        word= 'data'
+        word = 'data'
         data = ['d', 'a', 't', 'a']
         result = is_present(word, data)
         assert result == True
 
     def test_word_found_in_a_list_of_strings_written_in_reverse(self):
-        word= 'data'
+        word = 'data'
         data = ['a', 't', 'a', 'd']
         result = is_present(word, data)
         assert result == True
 
     def test_word_found_going_down(self):
-        word= 'data'
+        word = 'data'
         data = [
             ['d', 't', 'a', 'd'],
             ['a', 't', 'a', 'd'],
             ['t', 't', 'a', 'd'],
             ['a', 't', 'a', 'd'],
         ]
-        result = is_present(word, data)
+        result = handle_lists(word, data)
         assert result == True
--- word_search.py
+++ word_search.py
@@ -5,3 +5,16 @@ def is_present(word, data):
     data.reverse()
     backward = word in ''.join(data)
     return forward or backward
+
+
+def handle_lists(word, list_of_lists):
+    for lst in list_of_lists:
+        if is_present(word, lst):
+            return True
+    data = []
+    for i in range(0, len(list_of_lists)):
+        data.append([val[i] for val in list_of_lists])
+    for lst in data:
+        if is_present(word, lst):
+            return True
+
