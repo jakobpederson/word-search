dc086f21a06bcab1a4be7fe3ea58331e3ab3ccb3
jakobpederson <jakob.pederson+github@gmail.com>
2020-03-18 18:25:02 -0500

    Test: new test

--- test_word_search.py
+++ test_word_search.py
@@ -8,3 +8,9 @@ class TestWordSearch():
         data = ['d', 'a', 't', 'a']
         result = is_present(word, data)
         assert result == True
+
+    def test_word_found_in_a_list_of_strings_written_in_reverse(self):
+        word= 'data'
+        data = ['a', 't', 'a', 'd']
+        result = is_present(word, data)
+        assert result == True
