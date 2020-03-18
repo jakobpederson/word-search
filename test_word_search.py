94b56259bce28a3875f1e0e6aaf9a2a4976abc9c
jakobpederson <jakob.pederson+github@gmail.com>
2020-03-18 18:24:10 -0500

    Test: can find word in list

--- test_word_search.py
+++ test_word_search.py
@@ -1,7 +1,10 @@
+from word_search import is_present
+
 
 class TestWordSearch():
 
-    def test_x(self):
+    def test_word_found_in_a_list_of_strings(self):
+        word= 'data'
         data = ['d', 'a', 't', 'a']
-        result = is_present('data', data)
+        result = is_present(word, data)
         assert result == True
---
+++ word_search.py
@@ -0,0 +1,4 @@
+
+
+def is_present(word, data):
+    return word in ''.join(data)
