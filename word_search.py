c2d0abc754020d039a52e538618732e8daeaec18
jakobpederson <jakob.pederson+github@gmail.com>
2020-03-20 16:51:25 -0500

    Test: test passing

--- word_search.py
+++ word_search.py
@@ -39,18 +39,28 @@ def get_coordinates(name, lines):
 
 def word_search(data):
     result = {}
+    rotated_lines = rotate_lines(data[1:])
     for name in data[0]:
         result[name] = []
         for count, line in enumerate(data[1:]):
             result = get_name_and_coordinates(result, name, line, count)
+        for count, line in enumerate(rotated_lines):
+            result = get_name_and_coordinates(result, name, line, count, rotate=True)
     return result
 
-def get_name_and_coordinates(result, name, line, count):
-    coordinates = _get_coordinates(name, line, count)
-    reverse_coordinates = _get_coordinates(get_reverse_name(name), line, count)
+def get_name_and_coordinates(result, name, line, count, rotate=False):
+    coordinates = _get_coordinates(name, line, count, rotate=rotate)
+    reverse_coordinates = _get_coordinates(get_reverse_name(name), line, count, rotate=rotate)
     result[name].extend(coordinates)
     reverse_coordinates.reverse()
     result[name].extend(reverse_coordinates)
+    return result
+
+def rotate_lines(lines):
+    result = []
+    for i in range(len(lines[0])):
+        x = [line[i] for line in lines]
+        result.append(x)
     return result
 
 def get_reverse_name(name):
@@ -58,9 +68,12 @@ def get_reverse_name(name):
     reverse_name.reverse()
     return ''.join(reverse_name)
 
-def _get_coordinates(name, line, count):
+def _get_coordinates(name, line, count, rotate=False):
     coordinates = []
     if name in ''.join(line):
         start = ''.join(line).find(name)
-        coordinates = [(i, count) for i in range(start, start + len(name))]
+        if rotate:
+            coordinates = [(count, i) for i in range(start, start + len(name))]
+        else:
+            coordinates = [(i, count) for i in range(start, start + len(name))]
     return coordinates
