37ec82b70283d5b50a83aeec72796889a1d8868b
jakobpederson <jakob.pederson+github@gmail.com>
2020-03-26 16:36:23 -0500

    Refactor: refactoring

--- word_search.py
+++ word_search.py
@@ -7,32 +7,46 @@ def word_search(data):
     first = []
     for name in names:
         first = get_first_letters(name, lines)
-        up = []
-        down = []
-        left = []
-        right = []
-        up_right = []
-        up_left = []
-        down_right = []
-        down_left = []
-        for val in first:
-            up.append([(val[0], val[1] + i) for i in range(len(name))])
-            down.append([(val[0], val[1] - i) for i in range(len(name))])
-            right.append([(val[0] + i, val[1]) for i in range(len(name))])
-            left.append([(val[0] - i, val[1]) for i in range(len(name))])
-            up_right.append([(val[0] + i, val[1] + i) for i in range(len(name))])
-            up_left.append([(val[0] + i, val[1] - i) for i in range(len(name))])
-            down_right.append([(val[0] - i, val[1] + i) for i in range(len(name))])
-            down_left.append([(val[0] - i, val[1] - i) for i in range(len(name))])
-        all_combos = [up, down, right, left, up_right, up_left, down_right, down_left]
-        for combo in all_combos:
-            for coordinates in combo:
-                if name in _get_word(coordinates, lines, name):
-                    final[name] = coordinates
-                if name[::-1] in _get_word(coordinates, lines, name):
-                    coordinates.reverse()
-                    final[name] = coordinates
+        all_combos = get_all_combos(first, name)
+        for coordinate in all_combos:
+            if name in _get_word(coordinate, lines, name):
+                final[name] = coordinate
+            if name[::-1] in _get_word(coordinate, lines, name):
+                coordinate.reverse()
+                final[name] = coordinate
     return final
+
+def get_all_combos(first, name):
+    # up = []
+    # down = []
+    # left = []
+    # right = []
+    # up_right = []
+    # up_left = []
+    # down_right = []
+    # down_left = []
+    result = []
+    for val in first:
+        result.append([(val[0], val[1] + i) for i in range(len(name))])
+        result.append([(val[0], val[1] - i) for i in range(len(name))])
+        result.append([(val[0] + i, val[1]) for i in range(len(name))])
+        result.append([(val[0] - i, val[1]) for i in range(len(name))])
+        result.append([(val[0] + i, val[1] + i) for i in range(len(name))])
+        result.append([(val[0] + i, val[1] - i) for i in range(len(name))])
+        result.append([(val[0] - i, val[1] + i) for i in range(len(name))])
+        result.append([(val[0] - i, val[1] - i) for i in range(len(name))])
+        # up.append([(val[0], val[1] + i) for i in range(len(name))])
+        # down.append([(val[0], val[1] - i) for i in range(len(name))])
+        # right.append([(val[0] + i, val[1]) for i in range(len(name))])
+        # left.append([(val[0] - i, val[1]) for i in range(len(name))])
+        # up_right.append([(val[0] + i, val[1] + i) for i in range(len(name))])
+        # up_left.append([(val[0] + i, val[1] - i) for i in range(len(name))])
+        # down_right.append([(val[0] - i, val[1] + i) for i in range(len(name))])
+        # down_left.append([(val[0] - i, val[1] - i) for i in range(len(name))])
+    # all_combos = [up, down, right, left, up_right, up_left, down_right, down_left]
+    # for x in all_combos:
+        # print(x)
+    return result
 
 def get_first_letters(name, lines):
     first = []
