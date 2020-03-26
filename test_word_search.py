0a008f9c7fa6eca8b2732d809cd9f344b4cc4d4d
jakobpederson <jakob.pederson+github@gmail.com>
2020-03-25 19:32:03 -0500

    Refactor: remove old tests and code and refactor

--- test_word_search.py
+++ test_word_search.py
@@ -1,54 +1,8 @@
-from word_search import is_present, handle_lists, get_names, get_coordinates, word_search, get_diagonals
+from word_search import word_search
 from unittest import TestCase
 
 
 class TestWordSearch(TestCase):
-
-    def test_word_found_in_a_list_of_strings(self):
-        word = 'data'
-        data = ['d', 'a', 't', 'a']
-        result = is_present(word, data)
-        assert result == True
-
-    def test_word_found_in_a_list_of_strings_written_in_reverse(self):
-        word = 'data'
-        data = ['a', 't', 'a', 'd']
-        result = is_present(word, data)
-        assert result == True
-
-    def test_word_found_going_down(self):
-        word = 'data'
-        data = [
-            ['d', 't', 'a', 'd'],
-            ['a', 't', 'a', 'd'],
-            ['t', 't', 'a', 'd'],
-            ['a', 't', 'a', 'd'],
-        ]
-        result = handle_lists(word, data)
-        assert result == True
-
-    def test_word_not_present(self):
-        word = 'nothing'
-        data = [
-            ['d', 't', 'a', 'd'],
-            ['a', 't', 'a', 'd'],
-            ['t', 't', 'a', 'd'],
-            ['a', 't', 'a', 'd'],
-        ]
-        result = handle_lists(word, data)
-        assert result == False
-
-    def test_multiple_name_checks(self):
-        names = ['data', 'rat']
-        data = [['d', 'a', 't', 'a']]
-        result = get_names(names, data)
-        assert result[0] == names[0]
-
-    def test_get_coordinates(self):
-        name = 'data'
-        lines = [['d', 'a', 't', 'a']]
-        result = get_coordinates(name, lines)
-        assert result['data'] == [(0, 0), (0, 1), (0, 2), (0, 3)]
 
     def test_find_horizontally(self):
         new_data = self.data().copy()
@@ -58,15 +12,6 @@ class TestWordSearch(TestCase):
             'SCOTTY': [(0, 5), (1, 5), (2, 5), (3, 5), (4, 5), (5, 5)],
         }
         assert result == expected
-
-    # def test_find_horizontally_new_word(self):
-    #     new_data = self.data().copy()
-    #     new_data.insert(0, ["SCOTTY", "SULU"])
-    #     result = new_word_search(new_data)
-    #     expected = {
-    #         'SCOTTY': [(0, 5), (1, 5), (2, 5), (3, 5), (4, 5), (5, 5)],
-    #     }
-    #     assert result == expected
 
     def test_find_horizontally_reversed(self):
         new_data = self.data().copy()
@@ -100,31 +45,6 @@ class TestWordSearch(TestCase):
             'KHAN': [(5, 9), (5, 8),(5, 7), (5, 6)],
         }
         assert result == expected
-
-    def test_get_diagonals(self):
-        data = [
-            ['A', '1', 'X'],
-            ['B', '2', 'Y'],
-            ['C', '3', 'Z']
-        ]
-        expected_1 = [
-                ['X'],
-                ['1', 'Y'],
-                ['A', '2', 'Z'],
-                ['B', '3'],
-                ['C'],
-        ]
-        expected_2 = [
-                ['A'],
-                ['B', '1'],
-                ['C', '2', 'X'],
-                ['3', 'Y'],
-                ['Z'],
-        ]
-        diagonals = get_diagonals(data)
-        result = {x[0][0]: x for x in diagonals}
-        self.assertCountEqual(result['C'], expected_1)
-        self.assertCountEqual(result['A'], expected_2)
 
     def test_find_spock(self):
         new_data = self.data().copy()
--- word_search.py
+++ word_search.py
@@ -1,126 +1,3 @@
-import numpy
-from math import sqrt
-
-def is_present(word, data):
-    forward = word in ''.join(data)
-    data.reverse()
-    backward = word in ''.join(data)
-    return forward or backward
-
-
-def handle_lists(word, lines):
-    vertical_lines = []
-    if len(lines) > 1:
-        for i in range(0, len(lines)):
-            vertical_lines.append([val[i] for val in lines])
-    return any([check_lists(word, vals) for vals in (lines, vertical_lines)])
-
-
-def check_lists(word, lines):
-    for line in lines:
-        if is_present(word, line):
-            return True
-
-
-def get_names(names, lines):
-    result = []
-    for name in names:
-        if handle_lists(name, lines):
-            result.append(name)
-    return result
-
-
-def get_coordinates(name, lines):
-    if handle_lists(name, lines):
-        for key, line in enumerate(lines):
-            start = ''.join(line).find(name)
-            start = start if start > 0 else 0
-            return {name: [(key, i) for i in range(start, len(name))]}
-
-
-def word_search(data):
-    result = {}
-    rotated_lines = rotate_lines(data[1:])
-    for name in data[0]:
-        result[name] = []
-        result = find_word(data[1:], name, result)
-        result = find_word(rotated_lines, name, result, rotate=True)
-    return result
-
-
-def find_word(lines, name, result, rotate=False):
-    for count, line in enumerate(lines):
-        result = get_name_and_coordinates(result, name, line, count, rotate=rotate)
-        if result[name]:
-            return result
-        result = _get_diagonals(name, lines, result)
-    return result
-
-def _get_diagonals(name, lines, result):
-    diagonals = get_diagonals(lines)
-    for diagonal in diagonals:
-        for count_l, line in enumerate(diagonal):
-            if name in ''.join(line):
-                if count_l > len(line[0]):
-                    start_column = count_l - (len(lines) - 1)
-                else:
-                    start_column = count_l + len(lines)
-                start = ''.join(line).find(name)
-                real_start = start_column + start
-                if real_start < 0:
-                    real_start = (len(lines) - 1) + real_start
-                    beginning_of_word = (real_start, start)
-                    coordinates = [(beginning_of_word[0] - i, beginning_of_word[1] + i) for i in range(len(name))]
-                    result[name] = coordinates
-                else:
-                    beginning_of_word = (real_start, start)
-                    coordinates = [(beginning_of_word[0] + i, beginning_of_word[1] + i) for i in range(len(name))]
-                    result[name] = coordinates
-                return result
-    return result
-
-
-def get_name_and_coordinates(result, name, line, count, rotate=False):
-    coordinates = _get_coordinates(name, line, count, rotate=rotate)
-    reverse_coordinates = _get_coordinates(get_reverse_name(name), line, count, rotate=rotate)
-    result[name].extend(coordinates)
-    reverse_coordinates.reverse()
-    result[name].extend(reverse_coordinates)
-    return result
-
-
-def rotate_lines(lines):
-    result = []
-    for i in range(len(lines[0])):
-        x = [line[i] for line in lines]
-        result.append(x)
-    return result
-
-
-def get_reverse_name(name):
-    reverse_name = list(name)
-    reverse_name.reverse()
-    return ''.join(reverse_name)
-
-
-def _get_coordinates(name, line, count, rotate=False, diagonal=False):
-    coordinates = []
-    if name in ''.join(line):
-        start = ''.join(line).find(name)
-        if rotate:
-            coordinates = [(count, i) for i in range(start, start + len(name))]
-        else:
-            coordinates = [(i, count) for i in range(start, start + len(name))]
-    return coordinates
-
-
-def get_diagonals(lines):
-    matrix = numpy.array(lines)
-    shape = matrix.shape[0]
-    left_to_right = [numpy.diag(matrix, k=i).tolist() for i in range(-shape + 1, shape)]
-    matrix = numpy.flipud(matrix)
-    right_to_left = [numpy.diag(matrix, k=i).tolist() for i in range(-shape + 1, shape)]
-    return [right_to_left, left_to_right]
 
 
 def word_search(data):
@@ -129,11 +6,7 @@ def word_search(data):
     lines = data[1:]
     first = []
     for name in names:
-        for y, line in enumerate(lines):
-            for x, letter in enumerate(line):
-                if letter == name[0]:
-                    first.append((x, y))
-        print(first)
+        first = get_first_letters(name, lines)
         up = []
         down = []
         left = []
@@ -161,6 +34,14 @@ def word_search(data):
                     final[name] = yy
     return final
 
+def get_first_letters(name, lines):
+    first = []
+    for y, line in enumerate(lines):
+        for x, letter in enumerate(line):
+            if letter == name[0]:
+                first.append((x, y))
+    return first
+
 def _get_word(coords, lines, name):
     result = []
     if len(coords) == len(name):
@@ -170,40 +51,3 @@ def _get_word(coords, lines, name):
             except Exception:
                 pass
         return ''.join(result)
-
-
-
-def coordinates_check(coordinate, values):
-    result = []
-    for val in values:
-        if val[0] - coordinate[0] == 1:
-           result.append(val)
-        elif coordinate[0] - val[0] == 1:
-           result.append(val)
-        elif val[0] - coordinate[0] == 1:
-           result.append(val)
-        elif coordinate[0] - val[0] == 1:
-           result.append(val)
-    return result
-
-
-    # total = []
-    # for key, line in enumerate(answers):
-    #     for coord in line:
-    #         if key + 1 < len(answers):
-    #             for c in answers[key + 1]:
-    #                 first = coord[0] - c[0]
-    #                 second = c[0] - coord[0]
-    #                 third = coord[1] - c[1]
-    #                 fourth = c[1] - coord[1]
-    #                 if first is 1:
-    #                     total.append(coord)
-    #                     total.append(c)
-    #                 elif second is 1:
-    #                     total.append(coord)
-    #                     total.append(c)
-    #                 elif fourth is 1:
-    #                     total.append(coord)
-    #                     total.append(c)
-    # print(total)
-    return result
